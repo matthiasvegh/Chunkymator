f61bcc889ec4013b4c1d30ed7f626854f3f01f9a
Matthias Vegh <matyas.vegh@gmail.com>
2014-08-16 23:11:17 +0200

    Added basic test for camera interpolation in straight lines.

--- test.py
+++ test.py
@@ -12,5 +12,37 @@ class TestNormalizeFunction(unittest.TestCase):
 
         self.assertSequenceEqual(self.list, [0, 90, -70, -10, 45, 0])
 
+class MockSpline:
+	def __init__(self, *params):
+		self.list = []
+		for p in params:
+			self.list.append(p)
+
+	def __call__(self, index):
+		return self.list[index]
+
+class CameraAnglesTest(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_camera_angles_on_a_straight_line_should_look_forward(self):
+		xSpline = MockSpline(1, 2, 3, 4)
+		ySpline = MockSpline(0, 0, 0, 0)
+		zSpline = MockSpline(0, 0, 0, 0)
+
+		result = morph.getCameraAngles(xSpline, ySpline, zSpline, [0, 1, 2, 3])
+
+		(yaws, pitches) = result
+
+		self.assertEqual(len(yaws), 4)
+		self.assertEqual(len(pitches), 4)
+
+		for yaw in yaws:
+			self.assertAlmostEqual(yaw, 180)
+
+		for pitch in pitches:
+			self.assertAlmostEqual(pitch, 270)
+
 if __name__ == "__main__":
     unittest.main()
