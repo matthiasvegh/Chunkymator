eb02cd0470cd95e3b4779e3ee5f6f24edfe951e5
Matthias Vegh <matyas.vegh@gmail.com>
2014-08-22 20:25:56 +0200

    Preliminary version for dictionary_spline

--- dictionary_spline.py
+++ dictionary_spline.py
@@ -1,1 +1,23 @@
 import json
+import scipy.interpolate
+
+
+class VectorSpline:
+    def __init__(self, matrix, times):
+        self.matrix = matrix
+        self.times = times
+
+        self.splines = []
+        for parameterValues in self.matrix:
+            spline = scipy.interpolate.UnivariateSpline(
+                    self.times, parameterValues)
+            self.splines.append(spline)
+
+    def __call__(self, time):
+
+        #TODO: Make this pretty with a list comprehension
+        returnValue = []
+        for spline in self.splines:
+            returnValue.append(spline(time))
+        return returnValue
+
