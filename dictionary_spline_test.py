f7ebf18ee512b94ab8c9e7c996c9259ca8f76942
Matthias Vegh <matyas.vegh@gmail.com>
2014-08-25 21:40:38 +0200

    Added testcase for recursive dictionary interpolation.

--- dictionary_spline_test.py
+++ dictionary_spline_test.py
@@ -111,5 +111,23 @@ class VectorSplineTest(unittest.TestCase):
 			self.assertAlmostEqual(results[i][0]['y'], vector[i]['y'])
 			self.assertAlmostEqual(results[i][0]['z'], vector[i]['z'])
 
+	def test_recursive_dictionaries_should_be_interpolated_correctly(self):
+		vector = [
+				{'x':1, 'y':10, 'z':100, 'd':{'a':5}},
+				{'x':2, 'y':20, 'z':200, 'd':{'a':6}},
+				{'x':3, 'y':30, 'z':300, 'd':{'a':7}},
+				{'x':4, 'y':40, 'z':400, 'd':{'a':8}}]
+		times = [1, 2, 3, 4]
+		matrix = [vector]
+		v = VectorSpline(matrix, times)
+
+		results = [v(i) for i in times]
+
+		for i in range(4):
+			self.assertAlmostEqual(results[i][0]['x'], vector[i]['x'])
+			self.assertAlmostEqual(results[i][0]['y'], vector[i]['y'])
+			self.assertAlmostEqual(results[i][0]['z'], vector[i]['z'])
+			self.assertAlmostEqual(results[i][0]['d']['a'], vector[i]['d']['a'])
+
 if __name__ == "__main__":
 	unittest.main()
