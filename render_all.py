98213fdba0d1612ed28f5af40ab1b812d53f6f45
Matthias Vegh <matyas.vegh@gmail.com>
2014-09-14 19:22:59 +0200

    Added help messages and options for render_all.py

--- render_all.py
+++ render_all.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
+import subprocess
+import optparse
 
 def main():
-    pass
+
+    parser = optparse.OptionParser(
+            usage="%prog [options] scene1 scene2 ...",
+            description="Batch renderer for Chunky.")
+    parser.add_option("-t", "--target", dest="target",
+            default=1, type=int,
+            help="How many SPP to render frames at.",
+            metavar="NUM")
+    parser.add_option("-c", "--chunky-jar", dest="chunky",
+            type=str, help="Location of Chunky-Launcher.jar",
+            metavar="FILE")
+
+    (options, scenes) = parser.parse_args()
+    if options.chunky is None:
+        print "You must supply the path to Chunky-Launcher.jar"
+        return 1
+
+    for scene in scenes:
+        print scene
 
 if __name__ == "__main__":
     main()
