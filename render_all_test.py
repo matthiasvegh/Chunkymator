8986d74283b1d260b48f4b4a197adcfd380396d8
Matthias Vegh <matyas.vegh@gmail.com>
2014-09-18 21:23:08 +0200

    Get_command now uses Chunkypath, scene and target in commands.

--- render_all_test.py
+++ render_all_test.py
@@ -18,6 +18,21 @@ class Get_Commands_Test(unittest.TestCase):
 
 		self.assertEqual(len(result), 1)
 
+class Get_Command_Test(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_first_two_parameters_should_always_be_java_and_jar(self):
+		result = get_command("", "", 0)
+
+		self.assertEqual(result[0], "java")
+		self.assertEqual(result[1], "-jar")
+
+	def test_third_parameter_should_be_path_to_chunky(self):
+		result = get_command("", "Chunky", 0)
+
+		self.assertEqual(result[2], "Chunky")
 
 if __name__ == "__main__":
 	unittest.main()
